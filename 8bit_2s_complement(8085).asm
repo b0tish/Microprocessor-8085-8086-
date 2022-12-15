7d0fbb8277ca1f9ff8605fa0d110e936b01574c4
b0t <maharjan.saugat1+github@gmail.com>
2022-12-15 07:58:51 +0545

    Tweaked the program and added opcodes

--- 8bit_2s_complement(8085).asm
+++ 8bit_2s_complement(8085).asm
@@ -1,19 +1,48 @@
-mvi c, 00h
-lda 2051h
-mov b, a
-lda 2050h
-cma
-inr a
-add b
-jc go
-cma
-inr a
-jmp last
+;Addition using 2's complement
+	MVI C,00H
+    LDA 2051H
+    MOV B,A
+    LDA 2050H
+    CMA
+   	INR A
+    ADD B
+    JC LAST
+    CMA
+    INR A
+    INR C
+    LAST:STA 2052H
+       MOV A,C
+       STA 2053H
+       HLT
 
-go: inr c
-
-last: sta 2052h
-mov a, c
-sta 2053h
-
-hlt
+;Using opcodes
+	2050: 19  
+	2051: 10
+	2052: 00
+	2053: 00
+	2054: 0E
+	2055: 00
+	2056: 3A
+	2057: 51
+	2058: 20
+	2059: 47
+	205A: 3A
+	205B: 50
+	205C: 20
+	205D: 2F
+	205E: 3C
+	205F: 80
+	2060: DA
+	2061: 66
+	2062: 20
+	2063: 2F
+	2064: 3C
+	2065: 0C
+	2066: 32
+	2067: 52
+	2068: 20
+	2069: 79
+	206A: 32
+	206B: 53
+	206C: 20
+	206D: 76
